aa4237b38a897824bd392f684ed7d1616d815fc5
Working Rom <dpriyeshvk@live.co.uk>
2016-02-12 11:12:14 +0530

    exPower.py
    This gives the subtle difference between parameters and arguments.

---
+++ codeCademy/pyThon/exPower.py
@@ -0,0 +1,6 @@
+#this shows how arguments and parameters differ
+def power(base, exponent):  # Add your parameters here!
+    result = base**exponent
+    print "%d to the power of %d is %d." % (base, exponent, result)
+
+power(5,3)  # Add your arguments here!
