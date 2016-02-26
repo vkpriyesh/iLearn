c48f22c68af35ba3cbf341920a31429ae3f021d3
Working Rom <dpriyeshvk@live.co.uk>
2016-02-26 21:50:58 +0530

    lists and stuffs

---
+++ codeCademy/pyThon/listLength.py
@@ -0,0 +1,12 @@
+suitcase = [] 
+suitcase.append("sunglasses")
+
+# Your code here!
+suitcase.append("stuffs")
+suitcase.append("soaps")
+suitcase.append("sugar")
+
+list_length = len(suitcase) # Set this to the length of suitcase
+
+print "There are %d items in the suitcase." % (list_length)
+print suitcase
