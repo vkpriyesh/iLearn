1f6b54fbf4c37bb37c16c017d1d4b2fc59560061
Working Rom <dpriyeshvk@live.co.uk>
2016-03-04 23:10:06 +0530

    stockIt
    Dictionaries, list and loopings

---
+++ codeCademy/pyThon/superList.py
@@ -0,0 +1,24 @@
+shopping_list = ["banana", "orange", "apple"]
+
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+    
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+
+# Write your code below!
+def compute_bill(food):
+    total = 0
+    for items in food:
+        if stock[items] > 0:
+            total += prices[items]
+            stock[items] = stock[items] - 1
+    return total
