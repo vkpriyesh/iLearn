ed7445e916c803a2f47c936bcec8b99769976ec7
Working Rom <dpriyeshvk@live.co.uk>
2016-04-27 11:38:31 +0530

    updation

--- jq.js
+++ jq.js
@@ -25,6 +25,21 @@ lastNameLength = lastName.length;
 var firstLetterOfFirstName = "";
 var firstName = "Ada";
 
+///////////////////////////////////////////////
+//Array Usage:
+
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.push(["happy", "joy"]); 
+// ourArray now equals ["Stimpson", "J", "cat", ["happy", "joy"]]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+myArray.push(["dog", 3]);
+
+
 
 //here starts ARRAYing:
 // Example
