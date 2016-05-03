c3e310138dda5e5285b4cfb882bd9e492af3e13a
Working Rom <dpriyeshvk@live.co.uk>
2016-05-03 23:28:47 +0530

    SCOPE

--- jq.js
+++ jq.js
@@ -90,3 +90,24 @@ function reusableFunction(){
 
 reusableFunction();
 //FUNCTION ENDDS
+//SCOPING
+
+// Declare your variable here
+
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+  
+}
+
+// Only change code above this line
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
