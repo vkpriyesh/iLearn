875f6c562376be4742eb416532efa16ba336ee0d
Working Rom <dpriyeshvk@live.co.uk>
2016-05-04 22:54:24 +0530

    scoping

--- jq.js
+++ jq.js
@@ -111,3 +111,20 @@ function fun2() {
   }
   console.log(output);
 }
+
+//loc/Global
+
+function myLocalScope() {
+  'use strict';
+  var myVar = "asdf";
+  
+  console.log(myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+
+
+// Now remove the console log line to pass the test
+
